726203da5858c0b5354d583c290c2e92f264ca28
yasu_yamasaki <yasuyuki.yamasaki@finatext.com>
2021-05-28 12:29:27 +0900

    hello world

---
+++ src/main/kotlin/com/example/protocrestsample/HelloController.kt
@@ -0,0 +1,13 @@
+package com.example.protocrestsample
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RestController
+class HelloController {
+
+    @RequestMapping("/")
+    fun index(): String {
+        return "Greetings from Spring Boot!"
+    }
+}
--- src/main/kotlin/com/example/protocrestsample/ProtocRestSampleApplication.kt
+++ src/main/kotlin/com/example/protocrestsample/ProtocRestSampleApplication.kt
@@ -2,10 +2,11 @@ package com.example.protocrestsample
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["com.example.protocrestsample"])
 class ProtocRestSampleApplication
 
 fun main(args: Array<String>) {
-	runApplication<ProtocRestSampleApplication>(*args)
+    runApplication<ProtocRestSampleApplication>(*args)
 }
