7c9e61b8eb73442ceb7bde11779e5b8a3329930e
yasu_yamasaki <yasuyuki.yamasaki@finatext.com>
2021-05-28 12:22:39 +0900

    spring boot

---
+++ src/main/kotlin/com/example/protocrestsample/ProtocRestSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.protocrestsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProtocRestSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProtocRestSampleApplication>(*args)
+}
